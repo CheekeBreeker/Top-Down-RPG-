d849df872ab6cb65613aacaf3c7da5bc365c4d01
CheekeBreeker <72303991+CheekeBreeker@users.noreply.github.com>
2023-02-26 16:12:36 +0500

    PlayerLook

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,6 +19,9 @@ public class PlayerMovement : MonoBehaviour
     private float vertical;
     private float horizontal;
 
+    private Vector3 mousePosition;
+    public float lookSpeed = 2f;
+
     private void Awake()
     {
         _anim = GetComponent<Animator>();
@@ -33,15 +36,28 @@ public class PlayerMovement : MonoBehaviour
         _anim.SetFloat("vertical", vertical, 0.15f, Time.deltaTime);
         _anim.SetFloat("horizontal", horizontal, 0.15f, Time.deltaTime);
 
+        LookTo();
+
         transform.position += new Vector3(-vertical, 0f, horizontal) * _speed * Time.deltaTime;
-
-
-
-        RotationNormal();
     }
 
-    public void RotationNormal()
+    public void LookTo()
     {
+        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        //Vector3 difference = mousePosition - transform.position;
+        //difference.Normalize();
+        //float rotationY = Mathf.Atan2(difference.x, difference.z) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, 0f, rotationY);
+
+        //Plane playerPlane = new Plane(Vector3.up, transform.position); 		
+        //Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 		
+        //float hitdist = 0;  		
+        //if (playerPlane.Raycast (ray, out hitdist)) 
+        //{ 			
+        //    Vector3 targetPoint = ray.GetPoint (hitdist);  			
+        //    Quaternion targetRotation = Quaternion.LookRotation (targetPoint - transform.position);  			
+        //    transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, lookSpeed * Time.deltaTime); 		
+        //}
     }
 }
